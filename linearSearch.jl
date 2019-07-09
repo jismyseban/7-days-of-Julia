18c7fb133dccfebede9520b6016f8348eb6034ab
Jismy Sebastian <jismy.sebastian@ust-global.com>
2019-07-09 19:21:00 +0530

    added linear search

---
+++ linearSearch.jl
@@ -0,0 +1,12 @@
+function search(arr,n,x)
+    for i in 1:n
+        if(arr[i]==x)
+            return i
+        end
+    end
+    return -1    
+    
+end
+
+arr = [5,6,8,9,2]
+println(search(arr,5,78))
